130e2636dbd801a41e000d7c01d352c839b4baed
ElMaikina <mikesoto890@gmail.com>
2025-08-28 14:51:32 -0400

    actualizar modelo persona con fecha de nacimiento

--- my-app/src/models/Persona.tsx
+++ my-app/src/models/Persona.tsx
@@ -3,11 +3,13 @@ class Persona {
     Nombre: string;
     Sexo: string;
     RUT: string;
+    FechaDeNacimiento: string;
     constructor(json: any) {
         this.ID = json.ID;
         this.Nombre = json.Nombre;
         this.Sexo = json.Sexo;
         this.RUT = json.RUT;
+        this.FechaDeNacimiento = json.FechaDeNacimiento;
     }
 }
 export default Persona;

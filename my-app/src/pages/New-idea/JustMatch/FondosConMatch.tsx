3176cd9023ba362ecffee3c518cc207ea22e6dc8
javiwasabi <javiera.osoriom@usm.cl>
2025-09-02 16:08:54 -0400

    Animacion lista

--- my-app/src/pages/New-idea/JustMatch/FondosConMatch.tsx
+++ my-app/src/pages/New-idea/JustMatch/FondosConMatch.tsx
@@ -2,6 +2,7 @@ import React, { useState, useMemo, useEffect } from 'react';
 import NavBar from '../../../components/NavBar/navbar';
 import { Link, useNavigate } from 'react-router-dom';
 import { DisclaimerModal } from '../../../components/Shared/Disclaimer';
+import LoopAnimation from '../../../components/Shared/animationFrame';
 
 const colorPalette = {
   darkGreen: '#44624a',
@@ -83,6 +84,11 @@ const FondosconPorcentaje: React.FC = () => {
   const [categoriaFilter, setCategoriaFilter] = useState('Todas');
   const [sortBy, setSortBy] = useState<'compatibilidad' | 'alfabetico' | 'presupuesto'>('compatibilidad');
   const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [showAnimation, setShowAnimation] = useState(true);
+   useEffect(() => {
+    const timer = setTimeout(() => setShowAnimation(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     const projectData = localStorage.getItem('selectedProject');
@@ -116,6 +122,20 @@ const FondosconPorcentaje: React.FC = () => {
     return fondos;
   }, [searchTerm, categoriaFilter, sortBy]);
   const [showDisclaimer, setShowDisclaimer] = useState(true);
+ if (showAnimation) {
+  return (
+    <div className="min-h-screen bg-[#f1f5f9] flex flex-col">
+      <NavBar />
+      <div className="flex flex-col items-center justify-center flex-1 space-y-6">
+        <LoopAnimation />
+        <p className="text-xl sm:text-2xl font-semibold text-gray-700 animate-pulse">
+          Cargando...
+        </p>
+      </div>
+    </div>
+  );
+}
+
 
   return (
     <div className="min-h-screen bg-[#f1f5f9]">

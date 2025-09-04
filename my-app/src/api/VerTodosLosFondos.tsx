a57a9af44bcd80ac8383394e2ae0d6d84c28b962
Booker-catch <73317745+Booker-catch@users.noreply.github.com>
2025-09-04 06:48:55 -0400

    added APIs for last effort

---
+++ my-app/src/api/VerTodosLosFondos.tsx
@@ -0,0 +1,19 @@
+export async function VerTodosLosFondosIAAsync() {
+  try {
+    const response = await fetch(`https://uruguay-cigarettes-advised-answering.trycloudflare.com/api/v1/projects/all`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Error al obtener los datos');
+    }
+    const data = await response.json();
+    return data;
+  }
+  catch (error) {
+    console.error('Error en VerTodosLosInstrumentos:', error);
+    return [];
+  }
+}

8d66f9b3271dddfc4fca383ab998a5f003c65bc3
ElMaikina <mikesoto890@gmail.com>
2025-11-05 21:54:32 -0300

    arreglar drop down de regiones y otros datos en editar empresa

--- my-app/src/api/CambiarBeneficiario.tsx
+++ my-app/src/api/CambiarBeneficiario.tsx
@@ -1,7 +1,6 @@
 import Beneficiario from '../models/Beneficiario.tsx'
-import { useEffect, useState } from 'react';
 
-export async function CambiarBeneficiarioAsync(id: number, data: Beneficiario) {
+export async function CambiarBeneficiario(id: number, data: Beneficiario) {
   try {
     const response = await fetch(`https://backend.matchafunding.com/beneficiarios/${id}`, {
       method: 'PUT',
@@ -25,21 +24,10 @@ export async function CambiarBeneficiarioAsync(id: number, data: Beneficiario) {
     if (!response.ok) {
       throw new Error('Error al obtener los datos');
     }
-    const result: Beneficiario = await response.json();
-    return result;
+    const result: Beneficiario[] = await response.json();
+    return result[0];
   }
   catch (error) {
     console.error('Error en CambiarBeneficiario:', error);
   }
 }
-export function CambiarBeneficiario(id: number, data: Beneficiario) {
-  const [Beneficiario, setBeneficiario] = useState<Beneficiario>();
-
-  useEffect(() => {
-      CambiarBeneficiarioAsync(id, data).then((out) => {
-      setBeneficiario(out);
-      });
-  }, );
-  return Beneficiario;
-}
-export default CambiarBeneficiario;
